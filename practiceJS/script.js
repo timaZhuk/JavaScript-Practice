506442c3657532142845d6174ee13de944725362
timaZhuk <tima90zhuk@gmail.com>
2023-12-25 01:47:20 +0600

    Started revise basic of JS after 6 month delay

---
+++ practiceJS/script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+ /* 
+ //---------------------------------------------------
+
+ let str = '1234567'
+let arr = str.split('')
+console.log(arr)
+let sum = 0;
+for(let i=0; i<arr.length;i++ ){
+    sum +=Number(arr[i])
+
+}
+
+console.log(sum)
+
+
+
+ // how to use split, join, reverse
+let str = "12345678"
+let arr1 = str.split('')
+console.log(arr1) //['1', '2', '3', '4', '5', '6', '7', '8']
+// reverse the list of numbers
+let arr_rev = arr1.reverse()
+console.log(arr_rev) //['8', '7', '6', '5', '4', '3', '2', '1']
+// join to the new string
+let str_rev = arr_rev.join('')
+console.log(str_rev) //87654321
+
+ 
+ //----------------- JavaScript split method----------------------
+// The split() method splits a string into an array of substrings.
+let text = "How are you doing today buddy ?"
+const myArray = text.split(" ")
+console.log(myArray)
+console.log(myArray.length)
+
+//Different separaters
+let text_o = "How are=you=doing=today=buddy?"
+const equalArray = text_o.split("=")
+console.log(equalArray)
+let arr = text_o.split('=', 2);
+console.log(arr)s
+ 
+ //--------------------------------------------------------------
+ */
+
