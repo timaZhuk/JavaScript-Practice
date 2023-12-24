5b1339cf2578f14c3986105032fc2d1bbc429bf7
timaZhuk <tima90zhuk@gmail.com>
2023-12-25 01:43:56 +0600

    Started revise basic of JS after 6 month delay

---
+++ script.js
@@ -0,0 +1,31 @@
+let year = prompt("When was you born?", 1900)
+if (year==1990) 
+{
+    console.log("Yes, You are right")
+}
+else{
+    console.log("You aren't rigth!!")
+}
+
+//ghdakjcn fhkgri feghgekl sfhdlskaälna
+// let counter =1
+// let a=counter++;
+// console.log(a)
+
+// let number_en=" z1234 "
+// console.log("Null to zero",Number(number_en))
+
+// let isBulean=true
+// console.log(isBulean)
+
+// let value=true
+// value=String(value)
+// console.log(value)
+// console.log(typeof value)
+
+// let message="Hello, Tim"
+// message=1234578899023349858030303020000044455n
+// console.log(`Wow is super a big number  ${message}`)
+// let age=prompt("How old are you?", 33)
+// console.log(`My age is ${age}`)
+
